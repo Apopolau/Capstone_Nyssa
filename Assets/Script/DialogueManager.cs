78bc582ce2a3bb51be30b8f3ee1d96e683995adc
fatima17-09 <fatimahouran17@gmail.com>
2024-01-04 20:28:01 -0500

    Added Plant Buttons and Dialouge

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+
+    public Image characterIcon;
+    public TextMeshProUGUI dialogueArea;
+
+    private Queue<DialogueLine> lines;
+
+    public bool isDialogueActive = false;
+     public GameObject dialogueBox; // Reference to the entire dialogue box
+
+    public float typingSpeed = 0.5f;
+    void Start()
+    {
+        // Toggle visibility ON
+        SetVisibility(false);
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        lines = new Queue<DialogueLine>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        isDialogueActive = true;
+
+        // Toggle visibility ON
+        SetVisibility(true);
+
+        lines.Clear();
+
+        foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
+        {
+            lines.Enqueue(dialogueLine);
+        }
+
+        DisplayNextDialogueLine();
+    }
+
+    public void DisplayNextDialogueLine()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        DialogueLine currentLine = lines.Dequeue();
+
+        characterIcon.sprite = currentLine.character.icon;
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(currentLine));
+    }
+
+    IEnumerator TypeSentence(DialogueLine dialogueLine)
+    {
+        dialogueArea.text = "";
+        foreach (char letter in dialogueLine.line.ToCharArray())
+        {
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+
+        // Toggle visibility OFF
+        SetVisibility(false);
+    }
+
+    private void SetVisibility(bool isVisible)
+    {
+        
+        dialogueBox.SetActive(isVisible);
+    }
+}
