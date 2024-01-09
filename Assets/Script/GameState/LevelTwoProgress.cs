1391acd556b55f29a23b35317f334e64bbf683c2
Shepb26 <fizzlewizzlegp@gmail.com>
2024-01-09 15:38:53 -0500

    Started other levels

---
+++ Assets/Script/GameState/LevelTwoProgress.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTwoProgress : LevelProgress
+{
+    //Start of game condition, saved duck, destroyed oil spill
+    bool hasSavedHedgehog;
+    //Defeated the monster on the short path
+    bool hasFlowerSeeds;
+    //Cleared the rock slide
+
+    //Defeated the monster up the path
+    bool cleanedSidePath;
+    //Cleaned the water at the top of the path
+    bool hasShutOffDevice;
+
+    int treeCount;
+    int grassCount;
+    int flowerCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
