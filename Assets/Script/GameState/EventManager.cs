5767e7370a247ae00463340ef764e74e18b09c73
Shepb26 <fizzlewizzlegp@gmail.com>
2024-01-09 15:30:39 -0500

    Starting Level Evaluation Code
    Figuring out the architecture for level evaluation, outlining levels

--- Assets/Script/GameState/EventManager.cs
+++ Assets/Script/GameState/EventManager.cs
@@ -2,19 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelObjectives : MonoBehaviour
+public class EventManager : MonoBehaviour
 {
-    int duckFoodGoal;
-    int hedgehogFoodGoal;
-    int foxFoodGoal;
-    int humanFoodGoal;
 
-    int currentVeg;
-    int currentInsects;
-    int currentMeat;
-
-    bool hasCleanWater;
-    bool hasShelter;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +16,13 @@ public class LevelObjectives : MonoBehaviour
     void Update()
     {
         
+    }
+
+    protected void FlipTiles(List<GameObject> tiles)
+    {
+        foreach (GameObject tile in tiles)
+        {
+            tile.GetComponent<Cell>().terrainType = Cell.TerrainType.DIRT;
+        }
     }
 }
---
+++ Assets/Script/GameState/LevelOneProgress.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneProgress : LevelProgress
+{
+    //Start of game condition, saved duck, destroyed oil spill
+    bool hasSavedDuck;
+    //Defeated the monster on the short path
+    bool hasTallGrass;
+    //Cleared the rock slide
+
+    //Defeated the monster up the path
+    bool cleanedLongPath;
+    //Cleaned the water at the top of the path
+    bool hasTurnedOffPump;
+
+    int treeCount;
+    int grassCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
