bbbc3acebca7b917656241c3360c3306591f9e68
fatima17-09 <fatimahouran17@gmail.com>
2024-02-17 21:15:30 -0500

    Fixed UI facing camera (I thinkk..)

--- Assets/Script/UIToCamera.cs
+++ Assets/Script/UIToCamera.cs
@@ -14,11 +14,16 @@ public class UIToCamera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-      // Get the camera's rotation
+       // Get the camera's rotation
         Quaternion cameraRotation = mainCamera.transform.rotation;
 
         // Ensure that the pickupTarget is always facing the camera, including its rotation
         transform.LookAt(mainCamera.transform.position);
-        transform.rotation *= Quaternion.Euler(cameraRotation.eulerAngles.x, cameraRotation.eulerAngles.y, 0);
+
+        // Extract the Z-axis rotation from the camera's rotation
+        Quaternion rotationZ = Quaternion.Euler(0, cameraRotation.eulerAngles.y, cameraRotation.eulerAngles.z);
+
+        // Apply rotation around the Z-axis only
+        transform.rotation = rotationZ;
     }
 }
