b645604921c1a9dc7eb859b09533c7969c7b1e7b
fatima17-09 <fatimahouran17@gmail.com>
2024-02-21 22:46:44 -0500

    Added plant stat UI

--- Assets/Script/Grid System/Plant.cs
+++ Assets/Script/Grid System/Plant.cs
@@ -16,6 +16,13 @@ public class Plant : Creatable
     [SerializeField] private Stat storedSunlight;
     [SerializeField] private Stat storedWater;
     //[SerializeField] storedSunlight.current;
+    //private Stat storedSunlight;
+    //private Stat storedWater;
+
+    // Reference to the UI elements
+    public GameObject waterUI;
+    public GameObject sunlightUI;
+    
     public SpriteRenderer[] plantVisuals;
     private Cell tilePlantedOn;
     bool waterPlant = false;
@@ -27,6 +34,7 @@ public class Plant : Creatable
     public PlantStats.PlantStage currentPlantStage;
 
     private GameObject seed;
+    
 
     public new event System.Action<int, int> OnHealthChanged;
 
@@ -272,25 +280,64 @@ public class Plant : Creatable
 
     private void ResolveStats()
     {
-        //Change water state
-        if(storedWater.current < storedWater.max / 4)
+        
+        bool waterLow = storedWater.current < storedWater.max / 4;
+        bool sunlightLow = storedSunlight.current < storedSunlight.max / 4;
+
+        if (waterLow && !sunlightLow)
         {
-            storedWater.low = true;
+            // Water is low but sunlight is not, prioritize water UI
+            waterUI.SetActive(true);
+            sunlightUI.SetActive(false);
+            Debug.Log("Water is needed");
+        }
+        else if (!waterLow && sunlightLow)
+        {
+            // Sunlight is low but water is not, prioritize sunlight UI
+            waterUI.SetActive(false);
+            sunlightUI.SetActive(true);
+            Debug.Log("Sunlight is needed");
+        }
+        else if (waterLow && sunlightLow)
+        {
+            // Both water and sunlight are low, prioritize water UI
+            waterUI.SetActive(true);
+            sunlightUI.SetActive(false);
+            Debug.Log("Water is needed");
         }
         else
         {
+            // Both water and sunlight are not low, deactivate both UIs
+            waterUI.SetActive(false);
+            sunlightUI.SetActive(false);
+        }
+        
+        /*
+        // Change water state
+        if (storedWater.current < storedWater.max / 4)
+        {
+            storedWater.low = true;
+            waterUI.SetActive(true); // Activate water UI
+            Debug.Log("Water is needed");
+        }
+        else if (storedWater.current > storedWater.max / 4)
+        {
             storedWater.low = false;
+            waterUI.SetActive(false); // Deactivate water UI
+            Debug.Log("Water is not needed anymore");
         }
 
-        //Change sunlight state
+        // Change sunlight state
         if (storedSunlight.current < storedSunlight.max / 4)
         {
             storedSunlight.low = true;
+            sunlightUI.SetActive(true); // Activate sunlight UI
         }
         else
         {
             storedSunlight.low = false;
-        }
+            sunlightUI.SetActive(false); // Deactivate sunlight UI
+        } */
 
         //Change health state
         if (health.current < health.max / 4)
