1aff4916434cf389ca6d5a6b163cc459d612316f
Shepb26 <fizzlewizzlegp@gmail.com>
2024-04-07 23:10:03 -0400

    Finished most of the sound tutorial

--- Assets/Script/Sound/MusicManager.cs
+++ Assets/Script/Sound/MusicManager.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    MusicPlayer musicPlayer;
+    int activeLayerIndex = 0;
+    public int ActiveLayerIndex => activeLayerIndex;
 
+    MusicPlayer musicPlayer;
     public const int MaxLayerCount = 3;
+
+    float volume = 1;
+    public float Volume
+    {
+        get => volume;
+        private set
+        {
+            value = Mathf.Clamp(value, 0, 1);
+            volume = value;
+        }
+    }
 
     private static MusicManager instance;
     public static MusicManager Instance
@@ -49,7 +62,43 @@ public class MusicManager : MonoBehaviour
 
     public void PlayMusic(MusicEvent musicEvent, float fadeTime)
     {
-        Debug.Log("Play music");
+        if(musicEvent == null)
+        {
+            return;
+        }
         musicPlayer.Play(musicEvent, fadeTime);
+    }
+
+    public void StopMusic(float fadeTime)
+    {
+        musicPlayer.Stop(fadeTime);
+    }
+
+    public void IncreaseLayerIndex(float fadeTime)
+    {
+        int newLayerIndex = activeLayerIndex + 1;
+        newLayerIndex = Mathf.Clamp(newLayerIndex, 0, MaxLayerCount - 1);
+
+        if(newLayerIndex == activeLayerIndex)
+        {
+            return;
+        }
+
+        activeLayerIndex = newLayerIndex;
+        musicPlayer.FadeVolume(Volume, fadeTime);
+    }
+
+    public void DecreaseLayerIndex(float fadeTime)
+    {
+        int newLayerIndex = activeLayerIndex - 1;
+        newLayerIndex = Mathf.Clamp(newLayerIndex, 0, MaxLayerCount - 1);
+
+        if (newLayerIndex == activeLayerIndex)
+        {
+            return;
+        }
+
+        activeLayerIndex = newLayerIndex;
+        musicPlayer.FadeVolume(Volume, fadeTime);
     }
 }
--- Assets/Script/Sound/SoundPlayer.cs
+++ Assets/Script/Sound/SoundPlayer.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     List<AudioSource> layerSources = new List<AudioSource>();
+    List<float> sourceStartVolumes = new List<float>();
+    MusicEvent cMusicEvent = null;
+
+    Coroutine fadeVolumeRoutine = null;
+    Coroutine stopRoutine = null;
 
     private void Awake()
     {
@@ -34,16 +39,173 @@ public class SoundPlayer : MonoBehaviour
     public void Play(MusicEvent musicEvent, float fadeTime)
     {
         Debug.Log("Play music");
+        if(musicEvent == null)
+        {
+            return;
+        }
+
+        cMusicEvent = musicEvent;
         
         for(int i = 0; (i < layerSources.Count) && (i < musicEvent.MusicLayers.Length); i++)
         {
             if (musicEvent.MusicLayers[i] != null)
             {
+                layerSources[i].volume = 0;
                 layerSources[i].clip = musicEvent.MusicLayers[i];
+                layerSources[i].outputAudioMixerGroup = musicEvent.Mixer;
                 layerSources[i].Play();
             }
         }
-        
+
+        FadeVolume(MusicManager.Instance.Volume, fadeTime);
+    }
+
+    public void Stop(float fadeTime)
+    {
+        if(stopRoutine != null)
+        {
+            StopCoroutine(stopRoutine);
+        }
+        stopRoutine = StartCoroutine(StopRoutine(fadeTime));
+    }
+
+    IEnumerator StopRoutine(float fadeTime)
+    {
+        if(fadeVolumeRoutine != null)
+        {
+            StopCoroutine(fadeVolumeRoutine);
+        }
+
+        //blend the volume to 0, depending on blend type
+        if(cMusicEvent.SoundLayerType == SoundLayerType.ADDITIVE)
+        {
+            fadeVolumeRoutine = StartCoroutine(LerpSourceAdditiveRoutine(0, fadeTime));
+        }
+        else if (cMusicEvent.SoundLayerType == SoundLayerType.SINGLE)
+        {
+            fadeVolumeRoutine = StartCoroutine(LerpSourceSingleRoutine(0, fadeTime));
+        }
+
+        //wait for blend to finish
+        yield return fadeVolumeRoutine;
+        //stop all audio sources
+        foreach(AudioSource source in layerSources)
+        {
+            source.Stop();
+        }
+    }
+
+    public void FadeVolume(float targetVolume, float fadeTime)
+    {
+        targetVolume = Mathf.Clamp(targetVolume, 0f, 1f);
+        if (fadeTime < 0) fadeTime = 0;
+
+        if(fadeVolumeRoutine != null)
+        {
+            StopCoroutine(fadeVolumeRoutine);
+        }
+
+        if(cMusicEvent.SoundLayerType == SoundLayerType.SINGLE)
+        {
+            fadeVolumeRoutine = StartCoroutine(LerpSourceSingleRoutine(targetVolume, fadeTime));
+        }
+        if(cMusicEvent.SoundLayerType == SoundLayerType.ADDITIVE)
+        {
+            fadeVolumeRoutine = StartCoroutine(LerpSourceAdditiveRoutine(targetVolume, fadeTime));
+        }
+    }
+
+    IEnumerator LerpSourceSingleRoutine(float targetVolume, float fadeTime)
+    {
+        SaveSourceStartVolumes();
+
+        float newVolume;
+        float startVolume;
+
+        for (float elapsedTime = 0; elapsedTime <= fadeTime; elapsedTime += Time.deltaTime)
+        {
+            for (int i = 0; i < layerSources.Count; i++)
+            {
+                if(i == MusicManager.Instance.ActiveLayerIndex)
+                {
+                    startVolume = sourceStartVolumes[i];
+                    newVolume = Mathf.Lerp(startVolume, targetVolume, elapsedTime / fadeTime);
+                    layerSources[i].volume = newVolume;
+                }
+                else
+                {
+                    startVolume = sourceStartVolumes[i];
+                    newVolume = Mathf.Lerp(startVolume, 0, elapsedTime / fadeTime);
+                    layerSources[i].volume = newVolume;
+                }
+            }
+
+            yield return null;
+        }
+
+        for (int i = 0; i < layerSources.Count; i++)
+        {
+            if (i == MusicManager.Instance.ActiveLayerIndex)
+            {
+                layerSources[i].volume = targetVolume;
+            }
+            else
+            {
+                layerSources[i].volume = 0;
+            }
+        }
+    }
+
+    IEnumerator LerpSourceAdditiveRoutine(float targetVolume, float fadeTime)
+    {
+        SaveSourceStartVolumes();
+
+        float newVolume;
+        float startVolume;
+
+        for(float elapsedTime = 0; elapsedTime <= fadeTime; elapsedTime += Time.deltaTime)
+        {
+            for(int i = 0; i < layerSources.Count; i++)
+            {
+                //If in an active layer, fade in to target
+                if(i <= MusicManager.Instance.ActiveLayerIndex)
+                {
+                    startVolume = sourceStartVolumes[i];
+                    newVolume = Mathf.Lerp(startVolume, targetVolume, elapsedTime / fadeTime);
+                    layerSources[i].volume = newVolume;
+                }
+                //otherwise fade to 0 from current volume
+                else
+                {
+                    startVolume = sourceStartVolumes[i];
+                    newVolume = Mathf.Lerp(startVolume, 0, elapsedTime / fadeTime);
+                    layerSources[i].volume = newVolume;
+                }
+            }
+
+            yield return null;
+        }
+
+        for(int i = 0; i < layerSources.Count; i++)
+        {
+            if (i <= MusicManager.Instance.ActiveLayerIndex)
+            {
+                layerSources[i].volume = targetVolume;
+            }
+            else
+            {
+                layerSources[i].volume = 0;
+            }
+        }
+    }
+
+    private void SaveSourceStartVolumes()
+    {
+        sourceStartVolumes.Clear();
+        for (int i = 0; i < layerSources.Count; i++)
+        {
+            sourceStartVolumes.Add(layerSources[i].volume);
+        }
     }
 }
 
